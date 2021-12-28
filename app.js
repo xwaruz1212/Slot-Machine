4caa0a651b70ef0eff5c259e0bb7de3436db0323
Kacper Zurawski <kaczur190@gmail.com>
2021-12-28 11:44:06 +0100

    loading textures to Scene

--- app.js
+++ app.js
@@ -5,7 +5,45 @@ if (!PIXI.utils.isWebGLSupported()) {
 
 PIXI.utils.sayHello(type);
 
-const app = new PIXI.Application({width: 1500, height: 800, antialias: true, transparent: false, resolution: 1 });
+const app = new PIXI.Application({width: 1500, height: 800});
 document.body.appendChild(app.view);
 
-app.stage
+PIXI.Loader.shared
+.add ("images/duck.png")
+.add ("images/7.png")
+.add ("images/bells.png")
+
+.load(setup);
+
+function setup() {
+	const duck = new PIXI.Sprite(
+  PIXI.Loader.shared.resources["images/duck.png"].texture);
+  duck.x = 400;
+  duck.y = 300;
+  const Sduck = new PIXI.Sprite(
+    PIXI.Loader.shared.resources["images/duck.png"].texture);
+    Sduck.x = 600;
+    Sduck.y = 100;
+  const seven = new PIXI.Sprite(
+    PIXI.Loader.shared.resources["images/7.png"].texture);
+    seven.x = 600;
+    seven.y = 300;
+    const Sseven = new PIXI.Sprite(
+      PIXI.Loader.shared.resources["images/7.png"].texture);
+      Sseven.x = 800;
+      Sseven.y = 100;
+    const bells = new PIXI.Sprite(
+      PIXI.Loader.shared.resources["images/bells.png"].texture);
+      bells.x = 800;
+      bells.y = 300;
+      const Sbells = new PIXI.Sprite(
+        PIXI.Loader.shared.resources["images/bells.png"].texture);
+      Sbells.x = 400;
+      Sbells.y = 100;
+  
+  app.stage.addChild(seven,duck,bells,Sduck,Sseven,Sbells);
+
+}
+
+
+
