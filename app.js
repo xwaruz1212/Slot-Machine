eacc32176aa51c718461b27afe44079227a7b4f6
Kacper <kaczur190@gmail.com>
2022-01-12 22:41:01 +0100

    fresh start

--- app.js
+++ app.js
@@ -1,49 +1,31 @@
-let type = "WebGL";
-if (!PIXI.utils.isWebGLSupported()) {
-  type = "canvas";
-}
 
-PIXI.utils.sayHello(type);
 
-const app = new PIXI.Application({width: 1500, height: 800});
+const app = new PIXI.Application({backgroundColor: 0xff0000});
 document.body.appendChild(app.view);
 
-PIXI.Loader.shared
-.add ("images/duck.png")
-.add ("images/7.png")
-.add ("images/bells.png")
+app.loader
+    .add('images/duck.png', 'images/duck.png')
+    .add('images/7.png', 'images/7.png')
+    .add('images/bells.png', 'images/bells.png')
+    .add('images/bananas.png', 'images/bananas.png')
+    .add('images/cat.png', 'images/cat.png')
+    .add('images/cherry.png', 'images/cherry.png')
+    .load(onAssetsLoaded);
 
-.load(setup);
 
-function setup() {
-	const duck = new PIXI.Sprite(
-  PIXI.Loader.shared.resources["images/duck.png"].texture);
-  duck.x = 400;
-  duck.y = 300;
-  const Sduck = new PIXI.Sprite(
-    PIXI.Loader.shared.resources["images/duck.png"].texture);
-    Sduck.x = 600;
-    Sduck.y = 100;
-  const seven = new PIXI.Sprite(
-    PIXI.Loader.shared.resources["images/7.png"].texture);
-    seven.x = 600;
-    seven.y = 300;
-    const Sseven = new PIXI.Sprite(
-      PIXI.Loader.shared.resources["images/7.png"].texture);
-      Sseven.x = 800;
-      Sseven.y = 100;
-    const bells = new PIXI.Sprite(
-      PIXI.Loader.shared.resources["images/bells.png"].texture);
-      bells.x = 800;
-      bells.y = 300;
-      const Sbells = new PIXI.Sprite(
-        PIXI.Loader.shared.resources["images/bells.png"].texture);
-      Sbells.x = 400;
-      Sbells.y = 100;
-  
-  app.stage.addChild(seven,duck,bells,Sduck,Sseven,Sbells);
+const REEL_WIDTH = 160;
+const SYMBOL_SIZE = 150;
+
+
+function onAssetsLoaded() {
+    
+    const slotTextures = [
+        PIXI.Texture.from('images/duck.png'),
+        PIXI.Texture.from('images/7.png'),
+        PIXI.Texture.from('images/bells.png'),
+        PIXI.Texture.from('images/bananas.png'),
+        PIXI.Texture.from('images/cat.png'),
+        PIXI.Texture.from('images/cherry.png'),
+    ];
 
 }
-
-
-
