185b2a67399f89e0aa82839085d8918b9fc860ec
Kacper <kaczur190@gmail.com>
2022-01-13 12:30:05 +0100

    Spawning Symbols

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 
 
-const app = new PIXI.Application({backgroundColor: 0xff0000});
+const app = new PIXI.Application({backgroundColor: 0x000000});
 document.body.appendChild(app.view);
+
+app.renderer.resize(window.innerWidth, window.innerHeight);
+
 
 app.loader
     .add('images/duck.png', 'images/duck.png')
@@ -16,6 +19,8 @@ app.loader
 const REEL_WIDTH = 160;
 const SYMBOL_SIZE = 150;
 
+const center = 300
+
 
 function onAssetsLoaded() {
     
@@ -25,7 +30,29 @@ function onAssetsLoaded() {
         PIXI.Texture.from('images/bells.png'),
         PIXI.Texture.from('images/bananas.png'),
         PIXI.Texture.from('images/cat.png'),
-        PIXI.Texture.from('images/cherry.png'),
+        PIXI.Texture.from('images/cherry.png')
     ];
+    
+    //Displaying images in loop
+    for (let j=0 ; j < 3 ; j++){
+        for (let i=0 ; i < 4 ; i++){
+            
+            const symbol = new PIXI.Sprite(slotTextures[Math.floor(Math.random() * slotTextures.length)]);
+            symbol.y = 1.5* j * SYMBOL_SIZE;
+            symbol.x = 1.5* i * SYMBOL_SIZE + center;
+
+            console.log(symbol.y , " ", symbol.x);
+
+
+
+            app.stage.addChild(symbol);
+        }
+        
+    }
+
+
 
 }
+
+
+
